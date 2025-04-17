a076d387fba06034d781cab4c46fd18923b71a30
= <raushan.ranjan.863@gmail.co>
2025-04-17 22:05:19 +0530

    added new mood

--- fortune.py
+++ fortune.py
@@ -5,7 +5,7 @@ def main():
     admission_number = "21JE0751" 
 
     print(f"\nWelcome to {fname} {lname}'s Fortune Teller ({admission_number})\n")
-    mood = input("How are you feeling today? (happy/sad/neutral): ").strip().lower()
+    mood = input("How are you feeling today? (happy/sad/neutral/stressed): ").strip().lower()
 
     fortunes = {
         "happy": [
@@ -19,6 +19,10 @@ def main():
         "neutral": [
             "Still waters run deep. Something surprising is coming.",
             "No chaos = more clarity. Use this moment."
+        ],
+        "stressed":[
+            "Streesing won't help at all, keep calm",
+            "Stressing is the mother of all problems, keep calm"
         ]
     }
 
@@ -26,7 +30,7 @@ def main():
         message = random.choice(fortunes[mood])
         print(f"\nYour fortune: {message} \n")
     else:
-        print("\nHmm, I don’t know that mood. Try happy/sad/neutral.\n")
+        print("\nHmm, I don’t know that mood. Try happy/sad/neutral/stressed.\n")
 
 
 if __name__ == "__main__":
